3a659ca4600c047336ad44b8667d3f5373e13180
adanowitz <adanowit@calpoly.edu>
2020-04-22 08:41:36 -0700

    Adding missing starter code

---
+++ exp_eval.py
@@ -0,0 +1,34 @@
+from stack_array import Stack
+
+# You do not need to change this class
+class PostfixFormatException(Exception):
+    pass
+
+def postfix_eval(input_str):
+    '''Evaluates a postfix expression
+    
+    Input argument:  a string containing a postfix expression where tokens 
+    are space separated.  Tokens are either operators + - * / ** >> << or numbers.
+    Returns the result of the expression evaluation. 
+    Raises an PostfixFormatException if the input is not well-formed
+    DO NOT USE PYTHON'S EVAL FUNCTION!!!'''
+    pass
+
+def infix_to_postfix(input_str):
+    '''Converts an infix expression to an equivalent postfix expression
+
+    Input argument:  a string containing an infix expression where tokens are 
+    space separated.  Tokens are either operators + - * / ** >> << parentheses ( ) or numbers
+    Returns a String containing a postfix expression '''
+    pass
+
+
+def prefix_to_postfix(input_str):
+    '''Converts a prefix expression to an equivalent postfix expression
+    
+    Input argument:  a string containing a prefix expression where tokens are 
+    space separated.  Tokens are either operators + - * / ** >> << parentheses ( ) or numbers
+    Returns a String containing a postfix expression(tokens are space separated)'''
+    pass
+
+
